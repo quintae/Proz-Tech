054eb69c0024b693ba4ab5c2495bd9af6307f691
equintan <eunildes@hotmail.com>
2024-06-25 20:48:38 -0300

    add contador.py file

---
+++ contador.py
@@ -0,0 +1,20 @@
+print("Escolha a opção desejada:")
+print("(1) - Contador de Caracteres")
+print("(2) - Contador de Palavras")
+print("(3) - Sair")
+
+while True:
+    opcao= int(input("Opção: "))
+    if opcao == 3:
+        print("opcao 3 - saída do Programa.")
+        break
+    elif opcao == 1:
+        print("opcao 1 - contar caracteres")
+        texto = input("Informe o texto")
+        print("Tamanho: ", len(texto))
+        # break
+    else:
+        print("opcao 2 - contar palavras")
+        texto=input("Informe o texto: ")
+        print("Quantidade de palavras: ", len(texto.split()))
+        # break
